337ce64d7f2af115e8c3c481d7117321c1ed3dfb
EliAlkorta <elialkor@gmail.com>
2020-04-01 11:19:43 +0200

    Initial push

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from '../images/tarjetas-molonas.png';
-import LandingMain from './LandingMain';
-import Footer from './Footer';
+import logo from './logo.svg';
 import '../stylesheets/App.scss';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      
-        <div className="Landing-main">
-          <LandingMain image={logo}
-          />
-          <Footer
-          />
-        </div>
-      );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
