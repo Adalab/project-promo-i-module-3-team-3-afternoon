76f544ce67a8248f3f92f81bf3b0d12afcb639b0
EliAlkorta <elialkor@gmail.com>
2020-04-01 17:36:31 +0200

    create new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../images/tarjetas-molonas.png';
+import logoAdalab from '../images/logo-adalab.png';
+import LandingMain from './LandingMain';
+import Footer from './Footer';
 import '../stylesheets/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      
+        <div className="landing">
+          <LandingMain image={logo}
+          />
+          <Footer image={logoAdalab}
+          />
+        </div>
+      );
+    }
 }
+
 
 export default App;
---
+++ src/components/CardMaker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Preview from './Preview';
+// import Design from './Design';
+// import Fill from './Fill';
+// import Share from './Share';
+import defaultImage from '../images/queen.gif';
+// import {fetchCardData} from '../services/CardService';
+import '../stylesheets/App.scss';
+
+
+class CardMaker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    //body//
+}
+
+export default CardMaker;
