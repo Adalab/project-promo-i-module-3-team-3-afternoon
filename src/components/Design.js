67a018d43ad2cf4b36193dad7ad5dd3a3b192383
gavimar <gavimar182@gmail.com>
2020-04-08 11:26:06 +0200

    starting

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
+
+
 const Design = props => {
+  handleRadioChange(event){
+    const target = event.target;
+    props.handleRadioChange(target);
+  }
+
       return (
     
         <div className="color_selected">
            <p className="title special">COLORES</p>
             <ul>
             <li>
-             <input className="check" id="colorOne" type="radio" value="colorsCard" name="colorCard" palette="palette1"
-                 checked />
+             <input 
+             className="check" 
+             id="colorOne" 
+             type="radio" 
+             value="colorsCard" 
+             name="colorCard" 
+             palette="palette1"
+             onClick={this.handleRadioChange} />
              <label className="colorPalette">
                  <div className="palette containerColorPalette1"></div>
                  <div className="palette containerColorPalette2"></div>
