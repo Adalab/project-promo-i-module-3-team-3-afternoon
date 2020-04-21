7c5e29fe493ffbd226736fa904ca1cc3df429c41
cortesana <a.chdez2020@gmail.com>
2020-04-22 01:39:35 +0200

    Final just in case

--- src/components/validateForm.js
+++ src/components/validateForm.js
@@ -5,7 +5,6 @@ import Info from './Info.js'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.renderData = this.renderData.bind(this);
     this.handleInputValue = this.handleInputValue.bind(this);
     this.fileInput = React.createRef();
     this.validateForm = this.validateForm.bind(this);
@@ -23,8 +22,7 @@ class App extends React.Component {
       isValidated:false
     }
   }
-​
-  handleInputValue (inputName,inputValue) {
+​ handleInputValue (inputName,inputValue) {
     this.setState(prevState => {
       return {
         userInfo: {
@@ -36,15 +34,11 @@ class App extends React.Component {
     if(inputName === "palette") {
       this.setState({
         paletteChecked:inputValue
-      },() => this.validateForm())///admite un callback que se ejecuta después que se setea el estado, 
-                                  //aquí le paso una función que una vez que se ha seteado el estado
-                                  //ejecutará validando que ninguno de los campos vaya vacío
-​
+      },() => this.validateForm())
     } 
   }
 ​
   validateForm(){
-    //si una vez seteado el estado ninguno de los campos está vacío seteo a su vez mi booleano a true
     const { firstName, lastName, gender, age } = this.state.form;
     const isValidated = firstName.length > 0 && lastName.length > 0 && age.length > 0 && gender.length > 0;
     this.setState({isValidated:isValidated})
@@ -72,20 +66,16 @@ class App extends React.Component {
           email={userInfo.email}
           linkedin={userInfo.linkedin}
           github={userInfo.github}
-          // renderData = {this.renderData}
           handleInputValue = {this.handleInputValue}
           checked = {paletteChecked}
-          // isValidated = {this.state.isValidated}
         />
-        {/* {this.state.isSubmitted ?  */}
+        {}
           <Info 
             colorPalette={paletteChecked}
             firstName={userInfo.name}
             lastName={userInfo.job}
           />
-          {/* :
-          'no hay datos'
-        } */}
+          {}
       </div>
     );
   }
