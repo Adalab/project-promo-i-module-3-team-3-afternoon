29b0a11aaeb332c7e554ed361702be1dde70e5a3
cortesana <a.chdez2020@gmail.com>
2020-04-22 01:17:59 +0200

    Final Footer.js and FormInput.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
-
 class Footer extends React.Component {
     render() {
-    return (
-    <footer>
-        <div className="footer-wrapper">
-            <small className="copyright"><span className="first-letter">a</span>wesome profile-cards &copy;2020</small>
-            <div className="img-container">
-                <a href="https://adalab.es/"><img title="Logo de AdaLab" alt="Logo de AdaLab" src={this.props.image}/></a>
-            </div>
-        </div>
-    </footer> 
-  
-     )};
-
+        return (
+            <footer>
+                <div className="footer-wrapper">
+                    <small className="copyright"><span className="first-letter">a</span>wesome profile-cards &copy;2020</small>
+                    <div className="img-container">
+                        <a href="https://adalab.es/"><img title="Logo de AdaLab" alt="Logo de AdaLab" src={this.props.image}/></a>
+                    </div>
+                </div>
+            </footer> 
+    )};
 }
 
 export default Footer;
